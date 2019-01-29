5a983f5ae3f8de3bbe90a24719e71036a380c0ff
Luis Lara <luislpatino@gmail.com>
2019-01-29 17:55:05 -0600

    Implement argument parser
    Add filename argument.

--- olc/entry_points.py
+++ olc/entry_points.py
@@ -1,6 +1,15 @@
 """Definitions for all entry points."""
 
+import argparse
+
 
 def olc():
     """Run an experiment using a specification file."""
-    pass
+    parser = argparse.ArgumentParser(
+        description='Run an experiment using a specification file.'
+    )
+    parser.add_argument(
+        'filename',
+        help='path to the specifications file to be used.'
+    )
+    args = parser.parse_args()
