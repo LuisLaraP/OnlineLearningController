ce3dd27ecdda43a07f80de6609a90c6c2bb1f39b
Luis Lara <luislpatino@gmail.com>
2019-03-10 17:44:51 -0600

    Add the dense layer type

--- olc/neural_network.py
+++ olc/neural_network.py
@@ -1,7 +1,12 @@
 import tensorflow as tf
+from tensorflow.keras import layers
 
 
 def buildNetwork(name, specs):
 	with tf.variable_scope(name):
 		model = tf.keras.Sequential()
+		for layer in specs:
+			if layer['type'] == 'dense':
+				model.add(layers.Dense(layer['units']))
+	model.compile()
 	return model
