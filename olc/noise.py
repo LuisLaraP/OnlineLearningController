e86f6859a54026cdd03dc2cc3457e5bd23b6f88c
Luis Lara <luislpatino@gmail.com>
2019-03-13 20:40:19 -0600

    Create OrnsteinUhlenbeck noise class

---
+++ olc/noise.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+class OrnsteinUhlenbeck:
+
+	def __init__(self, ndim, dt, theta, sigma, mu=0):
+		self.dt = dt
+		self.theta = theta
+		self.sigma = sigma
+		self.mu = mu
+		self.x = np.zeros(ndim)
+
+	def step(self):
+		dx = self.theta * (self.mu - self.x) * self.dt + self.sigma * np.random.normal(self.mu, self.dt)
+		self.x += dx
+		return self.x
