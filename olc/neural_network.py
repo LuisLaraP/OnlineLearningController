c8ac900c5e4a37c0d9250a4feb7cb792bf2f9345
Luis Lara <luislpatino@gmail.com>
2019-04-11 19:56:22 -0500

    Add summaries for activations

--- olc/neural_network.py
+++ olc/neural_network.py
@@ -29,30 +29,35 @@ def buildNetwork(name, specs, inputs):
 	model.summaries = []
 	model.inputs = inputs
 	with tf.variable_scope(name):
-		for i, layer in enumerate(specs):
+		i = 1
+		for layer in specs:
 			funcName = '_{}Layer'.format(layer['type'])
 			globals()[funcName](i, layer, model)
+			if layer['type'] != 'input':
+				i += 1
 	model.summaries = tf.summary.merge(model.summaries)
 	return model
 
 
 def _denseLayer(i, specs, model):
 	nIn = model.output.shape[-1]
-	model.parameters.append(tf.get_variable(
-		'w' + str(i),
-		shape=(nIn, specs['units']),
-		initializer=getattr(tf.initializers, specs['initializer'])()
-	))
-	model.output = tf.matmul(model.output, model.parameters[-1])
-	model.parameters.append(tf.get_variable(
-		'b' + str(i),
-		shape=(specs['units']),
-		initializer=getattr(tf.initializers, specs['initializer'])()
-	))
-	model.output = model.output + model.parameters[-1]
-	model.summaries.append(tf.summary.histogram('z' + str(i), model.output))
-	if specs['activation'] != 'linear':
-		model.output = getattr(tf.nn, specs['activation'])(model.output)
+	with tf.variable_scope('layer' + str(i)):
+		model.parameters.append(tf.get_variable(
+			'w',
+			shape=(nIn, specs['units']),
+			initializer=getattr(tf.initializers, specs['initializer'])()
+		))
+		model.output = tf.matmul(model.output, model.parameters[-1])
+		model.parameters.append(tf.get_variable(
+			'b',
+			shape=(specs['units']),
+			initializer=getattr(tf.initializers, specs['initializer'])()
+		))
+		model.output = model.output + model.parameters[-1]
+		model.summaries.append(tf.summary.histogram('z', model.output))
+		if specs['activation'] != 'linear':
+			model.output = getattr(tf.nn, specs['activation'])(model.output)
+		model.summaries.append(tf.summary.histogram('a', model.output))
 
 
 def _inputLayer(i, specs, model):
