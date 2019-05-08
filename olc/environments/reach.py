c102772307c1a40941c06c47c834e9a9eb48d673
Luis Lara <luislpatino@gmail.com>
2019-05-08 10:37:47 -0500

    Use OpenAI Gym's Box for action and observation spaces

--- olc/environments/reach.py
+++ olc/environments/reach.py
@@ -8,11 +8,17 @@ The control signal is a vector with the velocity of each of the joints of the
 robot, and the reward is based on the current distance to the target position.
 """
 
+import numpy as np
+from gym.spaces import Box
+
 
 class Reach:
 
 	def __init__(self, settings, simulation):
-		pass
+		stateMin = np.concatenate((settings['robot']['workspace-min'], settings['robot']['joint-min']))
+		stateMax = np.concatenate((settings['robot']['workspace-max'], settings['robot']['joint-max']))
+		self.action_space = Box(-np.array(settings['robot']['max-velocities']), np.array(settings['robot']['max-velocities']))
+		self.observation_space = Box(stateMin, stateMax)
 
 	def close(self):
 		pass
