1a2ff71e67b40b6da3be84dab328d437805fde6a
Luis Lara <luislpatino@gmail.com>
2019-05-20 20:32:48 -0500

    Add a degree of freedom to reacher3

--- olc/environments/reacher3.py
+++ olc/environments/reacher3.py
@@ -1,8 +1,7 @@
 from roboschool.scene_abstract import SingleRobotEmptyScene
 from roboschool.gym_mujoco_xml_env import RoboschoolMujocoXmlEnv
-import gym, gym.spaces, gym.utils, gym.utils.seeding
 import numpy as np
-import os, sys
+
 
 class Reacher3(RoboschoolMujocoXmlEnv):
     '''
@@ -10,7 +9,7 @@ class Reacher3(RoboschoolMujocoXmlEnv):
     Similar to MuJoCo reacher.
     '''
     def __init__(self):
-        RoboschoolMujocoXmlEnv.__init__(self, 'reacher.xml', 'body0', action_dim=2, obs_dim=9)
+        RoboschoolMujocoXmlEnv.__init__(self, 'reacher3.xml', 'body0', action_dim=3, obs_dim=11)
 
     def create_single_player_scene(self):
         return SingleRobotEmptyScene(gravity=0.0, timestep=0.0165, frame_skip=1)
@@ -20,20 +19,24 @@ class Reacher3(RoboschoolMujocoXmlEnv):
         self.jdict["target_x"].reset_current_position(self.np_random.uniform( low=-self.TARG_LIMIT, high=self.TARG_LIMIT ), 0)
         self.jdict["target_y"].reset_current_position(self.np_random.uniform( low=-self.TARG_LIMIT, high=self.TARG_LIMIT ), 0)
         self.fingertip = self.parts["fingertip"]
-        self.target    = self.parts["target"]
+        self.target = self.parts["target"]
         self.central_joint = self.jdict["joint0"]
-        self.elbow_joint   = self.jdict["joint1"]
+        self.shoulder_joint = self.jdict["joint1"]
+        self.elbow_joint = self.jdict["joint2"]
         self.central_joint.reset_current_position(self.np_random.uniform( low=-3.14, high=3.14 ), 0)
+        self.shoulder_joint.reset_current_position(self.np_random.uniform( low=-3.14, high=3.14 ), 0)
         self.elbow_joint.reset_current_position(self.np_random.uniform( low=-3.14, high=3.14 ), 0)
 
     def apply_action(self, a):
         assert( np.isfinite(a).all() )
         self.central_joint.set_motor_torque( 0.05*float(np.clip(a[0], -1, +1)) )
         self.elbow_joint.set_motor_torque( 0.05*float(np.clip(a[1], -1, +1)) )
+        self.shoulder_joint.set_motor_torque( 0.05*float(np.clip(a[2], -1, +1)) )
 
     def calc_state(self):
         theta,      self.theta_dot = self.central_joint.current_relative_position()
         self.gamma, self.gamma_dot = self.elbow_joint.current_relative_position()
+        self.alpha, self.alpha_dot = self.shoulder_joint.current_relative_position()
         target_x, _ = self.jdict["target_x"].current_position()
         target_y, _ = self.jdict["target_y"].current_position()
         self.to_target_vec = np.array(self.fingertip.pose().xyz()) - np.array(self.target.pose().xyz())
@@ -47,6 +50,8 @@ class Reacher3(RoboschoolMujocoXmlEnv):
             self.theta_dot,
             self.gamma,
             self.gamma_dot,
+            self.alpha,
+            self.alpha_dot
             ])
 
     def calc_potential(self):
@@ -63,10 +68,11 @@ class Reacher3(RoboschoolMujocoXmlEnv):
         self.potential = self.calc_potential()
 
         electricity_cost = (
-            -0.10*(np.abs(a[0]*self.theta_dot) + np.abs(a[1]*self.gamma_dot))  # work torque*angular_velocity
-            -0.01*(np.abs(a[0]) + np.abs(a[1]))                                # stall torque require some energy
+            -0.10*(np.abs(a[0]*self.theta_dot) + np.abs(a[1]*self.gamma_dot) + np.abs(a[2]*self.alpha_dot))  # work torque*angular_velocity
+            -0.01*(np.abs(a[0]) + np.abs(a[1]) + np.abs(a[2])) # stall torque require some energy
             )
         stuck_joint_cost = -0.1 if np.abs(np.abs(self.gamma)-1) < 0.01 else 0.0
+        stuck_joint_cost += -0.1 if np.abs(np.abs(self.alpha)-1) < 0.01 else 0.0
         self.rewards = [float(self.potential - potential_old), float(electricity_cost), float(stuck_joint_cost)]
         self.frame  += 1
         self.done   += 0
