c996c7c72475338bb3d2c7e54e40cc95bc17b75c
Luis Lara <luislpatino@gmail.com>
2019-06-10 18:49:31 -0500

    Support an arbitrary number of joints in reacher7

--- olc/environments/reacher7.py
+++ olc/environments/reacher7.py
@@ -8,7 +8,11 @@ class Reacher7(RoboschoolMujocoXmlEnv):
 	definitionFile = 'reacher7.xml'
 
 	def __init__(self):
-		RoboschoolMujocoXmlEnv.__init__(self, self.definitionFile, 'body0', action_dim=1, obs_dim=4)
+		self.action_dim = 2
+		self.obs_dim = 7
+		RoboschoolMujocoXmlEnv.__init__(self, self.definitionFile, 'body0', action_dim=self.action_dim, obs_dim=self.obs_dim)
+		self.theta = np.zeros(self.action_dim)
+		self.theta_dot = np.zeros(self.action_dim)
 
 	def create_single_player_scene(self):
 		return SingleRobotEmptyScene(gravity=0.0, timestep=0.0165, frame_skip=1)
@@ -16,27 +20,33 @@ class Reacher7(RoboschoolMujocoXmlEnv):
 	def robot_specific_reset(self):
 		self.jdict["target_x"].reset_current_position(self.np_random.uniform(low=-0.47, high=0.47), 0)
 		self.jdict["target_y"].reset_current_position(self.np_random.uniform(low=-0.47, high=0.47), 0)
+		self.jdict["target_z"].reset_current_position(self.np_random.uniform(low=0, high=0.47), 0)
 		self.fingertip = self.parts["fingertip"]
 		self.target = self.parts["target"]
-		self.joint0 = self.jdict["joint0"]
-		self.joint0.reset_current_position(self.np_random.uniform(low=-3.14, high=3.14), 0)
+		for i in range(self.action_dim):
+			key = 'joint' + str(i)
+			limits = self.jdict[key].limits()[:2]
+			if limits[0] < limits[1]:
+				self.jdict[key].reset_current_position(np.random.uniform(low=limits[0], high=limits[1]), 0)
+			else:
+				self.jdict[key].reset_current_position(np.random.uniform(low=3.14, high=3.14), 0)
 
 	def apply_action(self, a):
 		assert(np.isfinite(a).all())
 		action = 0.05 * np.clip(a, -1, 1)
-		self.joint0.set_motor_torque(action[0])
+		for i in range(self.action_dim):
+			key = 'joint' + str(i)
+			self.jdict[key].set_motor_torque(action[i])
 
 	def calc_state(self):
-		self.theta, self.theta_dot = self.joint0.current_relative_position()
+		for i in range(self.action_dim):
+			key = 'joint' + str(i)
+			self.theta[i], self.theta_dot[i] = self.jdict[key].current_relative_position()
 		target_x, _ = self.jdict["target_x"].current_position()
 		target_y, _ = self.jdict["target_y"].current_position()
+		target_z, _ = self.jdict["target_z"].current_position()
 		self.to_target_vec = np.array(self.fingertip.pose().xyz()) - np.array(self.target.pose().xyz())
-		return np.array([
-			target_x,
-			target_y,
-			self.theta,
-			self.theta_dot
-		])
+		return np.append([target_x, target_y, target_z], [self.theta, self.theta_dot])
 
 	def calc_potential(self):
 		return -100 * np.linalg.norm(self.to_target_vec)
@@ -48,10 +58,7 @@ class Reacher7(RoboschoolMujocoXmlEnv):
 		state = self.calc_state()
 		potential_old = self.potential
 		self.potential = self.calc_potential()
-		electricity_cost = (
-			-0.10 * np.abs(a[0] * self.theta_dot)
-			- 0.01 * np.abs(a[0])
-		)
+		electricity_cost = -0.10 * np.dot(np.abs(a), np.abs(self.theta_dot)) - 0.01 * np.abs(a).sum()
 		self.rewards = [float(self.potential - potential_old), float(electricity_cost), 0]
 		self.frame += 1
 		self.done += 0
