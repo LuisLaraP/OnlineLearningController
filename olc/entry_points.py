bbafb92610f2c7fa8171a500c774551383092f14
Luis Lara <luislpatino@gmail.com>
2019-06-27 11:37:18 -0500

    Remove olc-test entry point

--- olc/entry_points.py
+++ olc/entry_points.py
@@ -3,44 +3,9 @@ import datetime
 import json
 
 import olc.environments as envs
-from olc.controller import Controller, Tester
+from olc.controller import Controller
 from olc.logger import Logger
 from olc.settings import getDefaults, merge
-
-
-def olc_test():
-	parser = argparse.ArgumentParser(
-		description='Run an experiment.'
-	)
-	parser.add_argument(
-		'settings',
-		help='path to the settings file.'
-	)
-	parser.add_argument(
-		'checkpoint_dir',
-		help='path to the checkpoint directory.'
-	)
-	args = parser.parse_args()
-
-	# Read settings
-	with open(args.settings, 'r') as settingsFile:
-		settings = json.load(settingsFile)
-
-	# Create environment
-	environment = envs.make(settings['task'])
-
-	time = datetime.datetime.now().time()
-	experimentName = '{}-Test-{:%H:%M}'.format(settings['task']['name'], time)
-	logger = Logger(experimentName)
-
-	# Create controller
-	defParams = getDefaults(__name__, 'params')
-	mergedParams = merge(defParams, settings)
-	controller = Tester(mergedParams, environment, args.checkpoint_dir, logger)
-
-	# Run
-	controller.run()
-	environment.close()
 
 
 def olc_train():
