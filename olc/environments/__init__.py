dbfe8b94304b511a05dfceeb01f7efcd994f71c1
Luis Lara <luislpatino@gmail.com>
2019-07-01 19:11:51 -0500

    Create motor perturbation for reacher2

--- olc/environments/__init__.py
+++ olc/environments/__init__.py
@@ -44,6 +44,11 @@ gym.envs.register(
 	entry_point='olc.environments.reacher2:Reacher2Joint',
 	max_episode_steps=100
 )
+gym.envs.register(
+	'Reacher2motor-v0',
+	entry_point='olc.environments.reacher2:Reacher2Motor',
+	max_episode_steps=100
+)
 
 gym.envs.register(
 	'Reacher3-v0',
--- olc/environments/reacher2.py
+++ olc/environments/reacher2.py
@@ -101,3 +101,38 @@ class Reacher2Joint(Reacher2):
         assert(np.isfinite(a).all())
         self.central_joint.set_motor_torque(0.05*float(np.clip(a[0], -1, +1)))
         self.elbow_joint.set_motor_torque(0)
+
+
+class Reacher2Motor(Reacher2):
+
+    definitionFile = 'reacher2motor.xml'
+
+    def robot_specific_reset(self):
+        self.jdict["target_x"].reset_current_position(self.np_random.uniform( low=-self.TARG_LIMIT, high=self.TARG_LIMIT ), 0)
+        self.jdict["target_y"].reset_current_position(self.np_random.uniform( low=-self.TARG_LIMIT, high=self.TARG_LIMIT ), 0)
+        self.fingertip = self.parts["fingertip"]
+        self.target    = self.parts["target"]
+        self.central_joint = self.jdict["joint0"]
+        self.central_joint.reset_current_position(self.np_random.uniform( low=-3.14, high=3.14 ), 0)
+
+    def apply_action(self, a):
+        assert(np.isfinite(a).all())
+        self.central_joint.set_motor_torque(0.05*float(np.clip(a[0], -1, +1)))
+
+    def calc_state(self):
+        theta,      self.theta_dot = self.central_joint.current_relative_position()
+        self.gamma, self.gamma_dot = (0.173, 0.)
+        target_x, _ = self.jdict["target_x"].current_position()
+        target_y, _ = self.jdict["target_y"].current_position()
+        self.to_target_vec = np.array(self.fingertip.pose().xyz()) - np.array(self.target.pose().xyz())
+        return np.array([
+            target_x,
+            target_y,
+            self.to_target_vec[0],
+            self.to_target_vec[1],
+            np.cos(theta),
+            np.sin(theta),
+            self.theta_dot,
+            self.gamma,
+            self.gamma_dot,
+            ])
